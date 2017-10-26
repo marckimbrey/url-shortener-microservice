28a5ee905249568f67dbc343252db431451705a2
marckimbrey <marckimbrey@gmail.com>
2017-10-26 18:34:30 +0100

    Added basic routing and insert and redirect functionality

--- index.js
+++ index.js
@@ -1,16 +1,81 @@
 const express = require('express');
 const app = express();
 const mongo = require("mongodb").MongoClient;
+var bodyParser = require('body-parser');
+var shortId = require('shortid');
+var validUrl = require('valid-url').isWebUri;
 
-const dburl = "mongodb://localhost:27017/testdb"
-app.get('/' , function (req, res) {
-  res.end("page is working")
-  mongo.connect(dburl, function (err, db) {
-    if (err)  return console.error(err)
+app.use(bodyParser.json());
 
-    console.log("mongo is running at " + dburl);
-    db.close()
-  });
+app.get('/', function(req, res) {// get homepage
+  res.send('load homepage');
+})
+
+const dburl = "mongodb://localhost:27017/urls"
+
+app.get('/', function(req, res) { // homepage
+
 });
+
+
+app.get('/new/:address(*)' , function (req, res) { // url to shorten
+  if (!validUrl(req.params.address)) {
+    res.end(JSON.stringify({error: 'URL is Invalid!'}))
+  }
+
+
+  mongo.connect(dburl, function (err, db) {
+    if (err)  throw err
+    db.collection('urls').findOne(
+      {fullUrl: req.params.address}, function (err, data) {
+      if (err) throw err;
+
+      if (data) { // if aready in db
+              console.log('data is not null');
+        db.close()
+
+        res.end(JSON.stringify(data));
+      } else { // add to db
+        var newEntry = {
+          fullUrl: req.params.address,
+          shortUrl: shortId.generate(req.params.address)
+        };
+        db.collection('urls').insert(newEntry, function (err, result) {
+          db.close()
+          res.end(Json.stringify(newEntry));
+        });
+      }
+
+    });
+  });
+  //res.end(shortId.generate(req.params.address))
+});
+
+app.get('/:address(*)', function (req, res) { // if shortened url
+  // if (!validUrl(req.params.address)) {
+  //   res.end(JSON.stringify({error: 'URL is Invalid!'}))
+  // }
+
+  mongo.connect(dburl, function (err, db) {
+    if (err)  throw err
+    db.collection('urls').findOne(
+      {shortUrl: req.params.address}, function (err, data) {
+      if (err) throw err;
+
+      if (data) { // if aready in db
+        db.close()
+        console.log('fullURL', data.fullUrl)
+        res.redirect(data.fullUrl);
+      } else { // add to db
+        res.end('That shortened URL is not in the database. To add use /new/')
+      }
+
+    });
+  });
+  // search db for shorturl
+
+  // if exists redirect to full url
+});
+
 
 app.listen(process.env.PORT || 3000);
