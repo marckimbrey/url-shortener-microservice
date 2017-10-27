cf189fc2108b4c04f2b211ee5f9edabac58ef066
marckimbrey <marckimbrey@gmail.com>
2017-10-27 17:38:02 +0100

    Added homepage

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ const mongo = require("mongodb").MongoClient;
 var bodyParser = require('body-parser');
 var shortId = require('shortid');
 var validUrl = require('valid-url').isWebUri;
+var hostName = 'http://localhost:3000/';
+var path = require('path');
 
 app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+
 
 app.get('/', function(req, res) {// get homepage
-  res.send('load homepage');
+  res.sendFile('./public/index.html');
 })
 
 const dburl = "mongodb://localhost:27017/urls"
@@ -38,28 +42,25 @@ app.get('/new/:address(*)' , function (req, res) { // url to shorten
       } else { // add to db
         var newEntry = {
           fullUrl: req.params.address,
-          shortUrl: shortId.generate(req.params.address)
+          shortUrl: hostName + shortId.generate(req.params.address)
         };
         db.collection('urls').insert(newEntry, function (err, result) {
           db.close()
-          res.end(Json.stringify(newEntry));
+          res.end(JSON.stringify(newEntry));
         });
       }
 
     });
   });
-  //res.end(shortId.generate(req.params.address))
 });
 
 app.get('/:address(*)', function (req, res) { // if shortened url
-  // if (!validUrl(req.params.address)) {
-  //   res.end(JSON.stringify({error: 'URL is Invalid!'}))
-  // }
+
 
   mongo.connect(dburl, function (err, db) {
     if (err)  throw err
     db.collection('urls').findOne(
-      {shortUrl: req.params.address}, function (err, data) {
+      {shortUrl: hostName + req.params.address}, function (err, data) {
       if (err) throw err;
 
       if (data) { // if aready in db
@@ -72,9 +73,6 @@ app.get('/:address(*)', function (req, res) { // if shortened url
 
     });
   });
-  // search db for shorturl
-
-  // if exists redirect to full url
 });
 
 
